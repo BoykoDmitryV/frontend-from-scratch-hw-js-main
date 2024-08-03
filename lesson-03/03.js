1edb0f6877520feb3dbdf1921a8f7b14c47b13c4
boykodmitryv <boykodmitryv@mail.ru>
2024-08-03 18:28:23 +0300

    2 commit

--- lesson-03/03.js
+++ lesson-03/03.js
@@ -1,5 +1,5 @@
 // Разработайте функцию findLargest, которая принимает три числа и возвращает наибольшее из них.
 
 function findLargest(value1, value2, value3) {
-    Math.max([value1[, value2[, value3]]])
+    return Math.max([value1[ value2[ value3]]])
 }
